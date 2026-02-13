32a7a5fedac14e8c3faa5f076701c85fd3929293
averym31 <averym31@nycstudents.net>
2026-02-13 08:04:14 -0500

    Update Rank.java

--- src/Rank.java
+++ src/Rank.java
@@ -9,7 +9,8 @@ public class Rank {
         this.handType = handType;
     }
 
-    public String[] RankHands(){
+    // ranks all the hands in the array
+    public String[] RankHands(boolean isWild){
         int count = 0;
         int alrcount = 0;
         String eString = "";
@@ -24,21 +25,28 @@ public class Rank {
             equalString = eString.split("\n");
             for (int p = 0; p < equalString.length; p++) {
                 equalString[p] = equalString[p].replace("10","v");
-                equalString[p] = equalString[p].replace("Jack","w");
+                // makes jack the weakest card if the Jack Wild rule applies
+                if (isWild){
+                    equalString[p] = equalString[p].replace("Jack","1");
+                }
+                else{
+                    equalString[p] = equalString[p].replace("Jack","w");
+                }
                 equalString[p] = equalString[p].replace("Queen","x");
                 equalString[p] = equalString[p].replace("King","y");
                 equalString[p] = equalString[p].replace("Ace","z");
             }
-            String[] handTypeSeperate = new String[count-alrcount];
-            for(int k = 0; k < handTypeSeperate.length; k++){
-                handTypeSeperate[k] = equalString[k+alrcount];
+            String[] handTypeSeparate = new String[count-alrcount];
+            for(int k = 0; k < handTypeSeparate.length; k++){
+                handTypeSeparate[k] = equalString[k+alrcount];
             }
-            Arrays.sort(handTypeSeperate);
-            for(int m = 0; m < handTypeSeperate.length; m++){
+            Arrays.sort(handTypeSeparate);
+            for(int m = 0; m < handTypeSeparate.length; m++){
                 equalString[m+alrcount] = "";
-                equalString[m+alrcount] = handTypeSeperate[m];
+                equalString[m+alrcount] = handTypeSeparate[m];
             }
             alrcount = count;
+            System.out.println(Arrays.toString(equalString));
         }
         return equalString;
     }
